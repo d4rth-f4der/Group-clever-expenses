3873f3f5b8eae28e6d935f729293f11b89224ca7
Oleksandr_Rudniev_PD14 <st9354750@stud.duikt.edu.ua>
2025-08-06 12:54:20 +0300

    feat: implemented user login through new API endpoint

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose.connect(MONGO_URI)
     .then(() => console.log('MongoDB connected...'))
     .catch(err => console.log('MongoDB connection error:', err));
 
-const generateToken = (id) => { // вот тут синтаксис = (id) => не понял
+const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
         expiresIn: '1h'
     });
@@ -34,7 +34,7 @@ app.post('/api/auth/register', async (req, res) => {
     try {
         const userExists = await User.findOne({ $or: [{ email }, { username }] });
         if (userExists) {
-            return res.status(409).json({message: "Username or email already exists"});
+            return res.status(409).json({ message: "Username or email already exists" });
         }
 
         const user = await User.create({
@@ -48,14 +48,42 @@ app.post('/api/auth/register', async (req, res) => {
                 _id: user._id,
                 username: user.username,
                 email: user.email,
-                token: generateToken(user._id), // вот тут забыл спросить. у нас же поле должно быть password, почему его не используем, а используем token?
+                token: generateToken(user._id),
             });
         } else {
-            return res.status(400).json({ message: 'Invalid user data'});
+            return res.status(400).json({ message: 'Invalid user data' });
         }
     } catch (error) {
         console.error('Registration error', error);
-        res.status(500).json({message: 'Server error during registration'});
+        res.status(500).json({ message: 'Server error during registration' });
+    }
+});
+
+app.post('/api/auth/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ message: "Required fields: email, password" });
+    }
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user || !(await user.matchPassword(password))) {
+            return res.status(401).json({ message: 'Invalid credentials (email/password)' });
+        }
+
+        res.status(200).json({
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            token: generateToken(user._id)
+        });
+    }
+
+    catch (error) {
+        console.error('Login error:', error);
+        return res.status(500).json({ message: "Server error during login" });
     }
 });
 
